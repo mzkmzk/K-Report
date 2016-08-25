ecd933b166af747a2930aff90009d384d82b4e3a
Mai ZhiKun <maizk@haplox.com>
2016-08-25 22:52:47 +0800

    fix

---
+++ Src/index.js
@@ -0,0 +1,19 @@
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+    var string = msg.toLowerCase();
+    var substring = "script error";
+    if (string.indexOf(substring) > -1){
+        console.log('Script Error: See Browser Console for Detail');
+    } else {
+        var message = [
+            'Message: ' + msg,
+            'URL: ' + url,
+            'Line: ' + lineNo,
+            'Column: ' + columnNo,
+            'Error object: ' + JSON.stringify(error)
+        ].join(' - ');
+
+        console.log(message);
+    }
+
+    return false;
+};
---
+++ Test/Test1.js
@@ -0,0 +1,3 @@
+function test(){
+	e.q.w;
+}
